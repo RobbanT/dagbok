b15e6e69291ca5782b6b7a60d54719c286238c1b
Roberto Tencic <roberto.tencic@outlook.com>
2023-12-15 21:49:27 +0100

    Layout och ikoner fixade

--- dagbok/src/main/resources/static/script.js
+++ dagbok/src/main/resources/static/script.js
@@ -1,4 +1,14 @@
 document.getElementById("dateClock").innerHTML = new Date().toLocaleDateString() + "<br/> " + new Date().toLocaleTimeString();
+let datePickers = document.getElementsByClassName("date-picker");
+datePickers.item(0).setAttribute("value", new Date().toLocaleDateString());
 setInterval(() => {
     dateClock.innerHTML = new Date().toLocaleDateString() + "<br/> " + new Date().toLocaleTimeString();
 }, 1000);
+
+datePickers.item(0).addEventListener("input", () => {
+    datePickers.item(0).style.color = "black";
+});
+
+datePickers.item(1).addEventListener("input", () => {
+    datePickers.item(1).style.color = "black";
+});
